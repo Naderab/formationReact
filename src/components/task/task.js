c07a074809f59ffd00bb2f69c677fed9275d7ff6
Naderab <abdellaouinader@gmail.com>
2022-07-14 14:38:08 +0100

    second session react-hook-form

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,41 +1,47 @@
-import './task.css'
-import { useState } from 'react';
-import TaskForm from '../taskForm/taskForm';
-function Task({title,duration,children,_id,deleteTask,updateTask}) {
-    const [isUpdated,setIsUpdated] = useState(true);
-    const [titleUpdated,setTitleUpdated]= useState(title)
-    const handleUpdateTask = ()=>{
-        console.log(titleUpdated)
-        updateTask({_id,title:titleUpdated,duration})
-        setIsUpdated(!isUpdated)
-    }
+import "./task.css";
+import { useState } from "react";
+import TaskForm from "../taskForm/taskForm";
+function Task({ title, duration, children, _id, deleteTask, updateTask }) {
+  const [isUpdated, setIsUpdated] = useState(true);
+  const [titleUpdated, setTitleUpdated] = useState(title);
+  const handleUpdateTask = () => {
+    console.log(titleUpdated);
+    updateTask({ _id, title: titleUpdated, duration });
+    setIsUpdated(!isUpdated);
+  };
 
-    return ( 
-    
-        <div className="task" style={{backgroundColor:'cyan'}}>
-    {isUpdated ? ( <><div className={`title ${ duration > 55 ? "customTask":""}`}>
+  return (
+    <div className="task" style={{ backgroundColor: "cyan" }}>
+      {isUpdated ? (
+        <>
+          <div className={`title ${duration > 55 ? "customTask" : ""}`}>
             {titleUpdated}
-        </div>
-        {duration}
-        <div className="actions">
-            <button type="submit" onClick={()=>deleteTask(_id)}>delete</button>
-            <button onClick={()=>setIsUpdated(!isUpdated)}>update</button>
-        </div>
-        <div>{children[0]}</div> 
-        </>):(<>
-      <ul>
-       
-      </ul>
-      <input
-        type="text"
-        name="title"
-        value={titleUpdated}
-        onChange={(e) => setTitleUpdated(e.target.value)}
-      />
-      <button type="submit" onClick={handleUpdateTask}>update</button>
-      
-    </>)}
-    </div>);
+          </div>
+          {duration}
+          <div className="actions">
+            <button type="submit" onClick={() => deleteTask(_id)}>
+              delete
+            </button>
+            <button onClick={() => setIsUpdated(!isUpdated)}>update</button>
+          </div>
+          <div>{children[0]}</div>
+        </>
+      ) : (
+        <>
+          <ul></ul>
+          <input
+            type="text"
+            name="title"
+            value={titleUpdated}
+            onChange={(e) => setTitleUpdated(e.target.value)}
+          />
+          <button type="submit" onClick={handleUpdateTask}>
+            update
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Task;
--- src/components/taskForm/taskForm.js
+++ src/components/taskForm/taskForm.js
@@ -1,10 +1,13 @@
 import { memo, useState } from "react";
+import { useForm } from "react-hook-form";
 import "./taskForm.css";
 function TaskForm(props) {
   // props.sayHello('Hello from TaskForm');
-  const [title, setTitle] = useState("");
   const addTask = "Add a new task";
   const steps = ["Enter the task title", "Click on add task"];
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => props.addTask(data.title);
+
   return (
     <>
       <ul>
@@ -12,15 +15,14 @@ function TaskForm(props) {
           return <li key={index}>{task}</li>;
         })}
       </ul>
-      <input
-        type="text"
-        name="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <button type="submit" onClick={()=> props.addTask(title)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("title", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.title && <span>This field is required</span>}
+      <button type="submit">
         {addTask}
       </button>
+      </form>
     </>
   );
 }
